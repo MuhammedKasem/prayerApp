420079d096b071df5bbb8f454db8c2ab1a5c3d64
Muhammed Kasem <muhammedkasem2002@gmail.com>
2022-09-10 01:28:56 -0400

    Initial commit with basic functionality and working API calls

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+  let [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.aladhan.com/v1/timingsByCity?city=Henderson&country=United%20States&method=2s', { method: 'GET', mode: 'cors' })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data['data']);
+      })
+      .catch(err => {
+        console.error(err)
+        data = null;
+      });
+  }, [])
+
+  function tConvert(time) {
+    // Check correct time format and split into components
+    time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+    if (time.length > 1) { // If time format correct
+      time = time.slice(1);  // Remove full string match value
+      time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+      time[0] = +time[0] % 12 || 12; // Adjust hours
+    }
+    return time.join(''); // return adjusted time or original string
+  }
+  return (data ?
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.prayerWrapper}>
+      <Text>{data.date.gregorian.weekday.en}</Text>
+      <Text>{tConvert(data.timings.Fajr)}</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </View> : <Text></Text>
   );
 }
 
@@ -17,4 +48,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  prayerWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  }
 });
---
+++ components/prayerTime.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native'
+
+const prayerTime = (props) => {
+  return (
+  <View style={styles.item}>
+      <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
